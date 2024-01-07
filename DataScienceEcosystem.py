1fccaf3a4beccc298d533fa06ef1363f34ce56eb
hasi1cool <155907975+hasi1cool@users.noreply.github.com>
2024-01-07 23:16:24 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+
+# In[2]:
+
+
+items = ['R', 'Python', 'Java']
+
+for index, item in enumerate(items, start=1):
+    print(f"{index}. {item}")
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# In[3]:
+
+
+items = ['TensorFlow', 'SciPy', 'PyTorch']
+
+for index, item in enumerate(items, start=1):
+    print(f"{index}. {item}")
+
+
+# | Data Science Tools|
+# | ------------------ |
+# | Jupyter Notebooks  |
+# | GitHub |
+# | Scikit-learn |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[6]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# **Objectives:**
+
+# * Popular languages that Data Scientists use
+# * Some of the commonly used libraries used by Data Scientists
+# * Data Science Tools
+# * Arithmetic expressions in Python
+# * converting minutes to hours
+
+# ## Author
+
+# Hasitha Premasiri
+
+# In[ ]:
+
+
+
+
